84d11873501e354e4ffa604b5063da7ece206671
MM674294 <mayamartey@gmail.com>
2025-12-03 21:59:38 +0000

    fix test teardown for ClothingPage widget tests

--- test/clothing_page_test.dart
+++ test/clothing_page_test.dart
@@ -7,9 +7,10 @@ void main() {
   testWidgets('ClothingPage displays and interacts correctly', (WidgetTester tester) async {
     await mockNetworkImagesFor(() async {
       // Optionally increase the test window size for scrolling
+       addTearDown(tester.binding.window.clearPhysicalSizeTestValue);
+
       tester.binding.window.physicalSizeTestValue = const Size(1200, 2000);
       tester.binding.window.devicePixelRatioTestValue = 1.0;
-      addTearDown(tester.binding.window.clearPhysicalSizeTestValue);
       addTearDown(tester.binding.window.clearDevicePixelRatioTestValue);
 
       await tester.pumpWidget(
