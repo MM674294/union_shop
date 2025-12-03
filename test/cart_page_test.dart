6e89031acefc0c8c01d47eabb059764abcc1f18a
MM674294 <mayamartey@gmail.com>
2025-12-03 22:19:39 +0000

    refactor: comment out unused import in cart_page_test.dart

--- test/cart_page_test.dart
+++ test/cart_page_test.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:union_shop/cart_page.dart';
-import 'package:union_shop/cart_state.dart';
+//import 'package:union_shop/cart_state.dart';
 import 'package:network_image_mock/network_image_mock.dart';
+import 'package:union_shop/main.dart';
 
 void main() {
   setUp(() {
