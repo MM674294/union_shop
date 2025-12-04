5b2eeadeff3051ba7e0224d853f6f4895a97cb9b
MM674294 <mayamartey@gmail.com>
2025-12-04 00:30:42 +0000

    add initial test setup for Halloween bags page

---
+++ test/halloween_bag_page_test.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:union_shop/halloween_bags_page.dart';
+import 'package:network_image_mock/network_image_mock.dart';
